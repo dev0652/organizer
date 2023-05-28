32aad31516b03bd7cbdda821b5c69bc6de49b69b
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-05-28 03:40:05 +0300

    tmp

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
     <Toaster
       toastOptions={{
-        duration: 3000,
+        duration: 2000,
         style: {
           maxWidth: '100%',
           fontSize: '1.3rem',
