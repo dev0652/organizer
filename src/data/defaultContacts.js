b415de0ac67b37afc570e6aab0275c1e5f52626a
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-06-11 12:43:23 +0300

    fixed writing to localStorage

--- src/data/defaultContacts.js
+++ src/data/defaultContacts.js
@@ -3,7 +3,6 @@ const defaultContacts = [
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  { id: 'id-5', name: 'test', number: '227-91-26' },
 ];
 
 export default defaultContacts;
