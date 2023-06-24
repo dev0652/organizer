c6003d30097c908667e52fa673901f39e3b06063
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-06-24 11:28:47 +0000

    Deploying to gh-pages from @ dev0652/goit-react-hw-08-phonebook@bd6c27a9fdc91631d3a14d0306e93d1b79f19d90 🚀

---
+++ static/js/415.383023fb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,o,n){n.r(o),n.d(o,{default:function(){return r}});n(2791);var t=n(184);function r(){return(0,t.jsx)("div",{children:"Welcome to our homepage"})}}}]);
+//# sourceMappingURL=415.383023fb.chunk.js.map
