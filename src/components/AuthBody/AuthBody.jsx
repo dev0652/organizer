a6e1b9214acbf4a73124546b5048bea857edcd6a
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-07-01 10:21:43 +0300

    tmp

--- src/components/AuthBody/AuthBody.jsx
+++ src/components/AuthBody/AuthBody.jsx
@@ -18,7 +18,7 @@ export default function AuthBody({ children }) {
           <BannerText>From everywhere</BannerText>
         </BannerTextWrapper>
 
-        {/* <BannerImage
+        {/* <img
           src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/aa8625109287767.5fd08439c7676.jpg"
           srcset="https://mir-s3-cdn-cf.behance.net/project_modules/disp/aa8625109287767.5fd08439c7676.jpg 600w, https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/aa8625109287767.5fd08439c7676.jpg 1200w, https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/aa8625109287767.5fd08439c7676.jpg 1400w, https://mir-s3-cdn-cf.behance.net/project_modules/fs/aa8625109287767.5fd08439c7676.jpg 1920w, https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/aa8625109287767.5fd08439c7676.jpg 2800w"
           sizes="(max-width: 2800px) 100vw, 2800px"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,29 @@ const Wrapper = styled.div`
   height: 100vh;
 
   display: flex;
-  justify-content: center;
-  /* align-items: center; */
-  padding-top: 20vh;
+  flex-direction: column;
+  gap: 20px;
+  /* justify-content: center; */
+  align-items: center;
+  padding-top: 10vh;
 
   color: white;
 
-  background-color: #6684ff;
-  background: linear-gradient(105.62deg, #524abe -7.88%, #6684ff 100%);
+  /* background-color: #6684ff;
+  background: linear-gradient(105.62deg, #524abe -7.88%, #6684ff 100%); */
+
+  background-color: #660dd9;
+`;
+
+const HomeImage = styled.img`
+  max-width: 80vh;
+`;
+
+const HomeText = styled.p`
+  font-size: 1.6rem;
+  display: block;
+  text-align: center;
+  max-width: 70vw;
 `;
 
 export default function Home() {
@@ -25,7 +40,19 @@ export default function Home() {
       </Helmet>
 
       <Wrapper>
-        <h1>Home of your contacts</h1>
+        <h1>Forget about Google</h1>
+
+        <HomeText>
+          Create, manage and access your contacts with piece of mind without
+          fear of being tracked by the Evil Corporation.
+        </HomeText>
+
+        <HomeImage
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/aa8625109287767.5fd08439c7676.jpg"
+          srcset="https://mir-s3-cdn-cf.behance.net/project_modules/disp/aa8625109287767.5fd08439c7676.jpg 600w, https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/aa8625109287767.5fd08439c7676.jpg 1200w, https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/aa8625109287767.5fd08439c7676.jpg 1400w, https://mir-s3-cdn-cf.behance.net/project_modules/fs/aa8625109287767.5fd08439c7676.jpg 1920w, https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/aa8625109287767.5fd08439c7676.jpg 2800w"
+          sizes="(max-width: 2800px) 100vw, 2800px"
+          alt="UI Design 3D Illustration"
+        />
       </Wrapper>
     </>
   );
